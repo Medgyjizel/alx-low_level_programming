e75ba38a957e5abb3fdffd8e3212008d9b29edef
Medgy <med@gmail.com>
2023-03-01 16:55:01 -0800

    correction

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,13 @@
 
 
 	/**
-	 * _isalpha - checks for alphabetic character
-	 * @c: the character to be checked
-	 * Return: 1 if c is a letter, 0 otherwise
+	 * _isalpha  checks for alphabetic character
+	 * @c the character to be checked
+	 * Return 1 if c is a letter, 0 otherwise
 	 */
 int _isalpha(int c)
 {
-		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 			return (1);
 		else
 			return (0);
