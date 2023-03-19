9b8d74645db91db6aa1df02222e5860506a60802
Medgy <med@gmail.com>
2023-03-19 11:15:00 -0700

    final

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* @argc: Count arguments
+* main - Print the name of the program
+* @argv: Arguments
+*/
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+return (0);
+}
